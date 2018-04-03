e65d0a4c85b5c709338017f7c6daa0c3b99a3cce
Maxrozvora <rozvora.max@gmail.com>
2018-04-03 23:46:57 +0300

    All template witout adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,10 @@
 $(function() {
 
-	// Custom JS
+	$('.contact__more').on('click', function() {
+		$('.tooltip').toggleClass('d-none');
+	});
+	$('.contact__more').on('focusout', function() {
+		$('.tooltip').addClass('d-none');
+	});
 
 });
